c5ec79af1cf4e24dfb8c4d861f2ba7ba0fc5c9f4
leonidus96 <carlo.santoro2@studenti.unina.it>
2024-02-22 16:12:12 +0100

    modificato script set-env eliminando parti non piu necessarie.

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-import {ApiMockService} from "../app/services/api/api.mock.service";
-
-export const environment = {
-  apiScheme: '',
-  apiHost: '',
-  apiPort: '',
-  apiService:ApiMockService
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
-import {ApiService} from "../app/services/api/api.service";
 
 export const environment = {
     apiScheme: 'http',
     apiHost: 'localhost',
     apiPort: '9001',
-    apiService:ApiService
 }
